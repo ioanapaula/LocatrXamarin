92a4be21c61ca5cf6cac4fd9dcc321755af91b36
Ioana-Paula Stroe <stroe.ioanapaula@gmail.com>
2019-04-23 08:45:44 +0300

    Initial Commit

---
+++ LocatrXamarin/Activities/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace LocatrXamarin
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
